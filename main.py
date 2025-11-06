1a9c69c3409127a22977bc4c8353b83a3fa614f0
SergioPlayerOne <sergiolosan11@gmail.com>
2025-11-06 14:50:31 +0100

    Added add command without task saving

--- main.py
+++ main.py
@@ -3,12 +3,37 @@ import argparse
 # create the top level parser
 parser = argparse.ArgumentParser(
     prog='Terminal task manager',
-    description='A simple and lightweight task manager for the terminal')
-command_parser = parser.add_subparsers()
+    description='A simple and lightweight task manager for the terminal'
+)
+command_parser = parser.add_subparsers(
+    title='commands',
+    description='specify which action to take on the tasks',
+    dest="command"
+)
 
 # the 'add' parser
 add_parser = command_parser.add_parser(
-    name='add')
+    name='add',
+    help="add a new task with title with the specified optional parameters"
+)
+add_parser.add_argument(
+    '-t', '--title',
+    help='the title of the task to add'
+)
+add_parser.add_argument(
+    '-d', '--description',
+    help='the description of the task'
+)
 
-# Argument tests
-parser.parse_args(["-h"])
+# Handles the arguments parsed
+args = parser.parse_args()
+if args.command == "add":
+    newTask = {
+        'Title': None,
+        'Description': None
+    }
+    if args.title:
+        newTask["Title"] = args.title
+    if args.description:
+        newTask["Description"] = args.description
+    print("The task " + str(newTask) + " was created successfully")
