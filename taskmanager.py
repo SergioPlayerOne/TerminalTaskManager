357a086237da5b9bd0e070ed186c464a6b801639
SergioPlayerOne <sergiolosan11@gmail.com>
2025-11-07 19:00:34 +0100

    Added simple database initialization and add_task() method, with room for improvement

---
+++ taskmanager.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+connecction = sqlite3.connect("tasks")
+cursor = connecction.cursor()
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS task (" \
+    "   title TEXT, " \
+    "   description TEXT" \
+    ")" \
+)
+
+# Gets a table and prints it row by row
+def log_tasks():
+    rows = cursor.execute("SELECT * FROM task").fetchall()
+    if len(rows) == 0:
+        print("There are no tasks stored in the database")
+    else:  
+        for row in rows:
+            print(row)
+
+# Adds a new task
+def add_task(title: str, description: str):
+    cursor.execute(
+        "INSERT INTO task (id, title, description) VALUES (?, ?, ?)",
+        (title, description)
+    )
+    connecction.commit()
